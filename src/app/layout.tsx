22dff6d2ff9d6e2ba34d197e7eb6d637f13b9c69
e759ksh <e759ksh@naver.com>
2023-12-17 17:35:52 +0900

    아이콘이랑 불필요한 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,6 @@ import Footer from "@/components/Footer";
 import { AuthContextProvider } from "@/context/AuthContext";
 import { DarkModeProvider } from "@/components/DarkModeProvider";
 import ScrollTop from "@/components/ScrollTop";
-import Head from "next/head";
-import { IoAnalytics } from "react-icons/io5";
 
 const sans = Noto_Sans_KR({ subsets: ["latin"] });
 
@@ -34,19 +32,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={sans.className}>
-      <Head>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
-        />
-      </Head>
       <body className="flex flex-col w-full">
         <AuthContextProvider>
           <DarkModeProvider>
             <Header />
             <main className="grow px-4 max-w-screen-xl mx-auto w-full">
               {children}
-              <IoAnalytics />
             </main>
             <Footer />
             <ScrollTop />
